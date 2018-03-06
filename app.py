a2d32260dea3e3813312063bb1892c26a4b33271
leesangyoon <pinetree408@gmail.com>
2018-03-06 18:54:49 +0900

    Add flake8 for convention & Update convention at main

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from threading import Lock
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-import sys
-import os
-import json
 import time
 import watch_sensor
 import simulation
-import time
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+
+def current_milli_time():
+    return int(round(time.time() * 1000))
+
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -23,16 +22,18 @@ thread_lock = Lock()
 simulation.init()
 
 windows = {
-        1: [], #acc
-        4: [], #gyr
-        10 : [] #lin
-        }
+    1: [],  # acc
+    4: [],  # gyr
+    10: []  # lin
+}
 
 start_time = None
 
+
 def background_thread(sens_type, sens_time, sens_x, sens_y, sens_z):
     global windows, start_time
-    prepared = watch_sensor.data_parser(sens_type, sens_time, sens_x, sens_y, sens_z, windows)
+    prepared = watch_sensor.data_parser(
+        sens_type, sens_time, sens_x, sens_y, sens_z, windows)
     if prepared:
         if start_time is None:
             start_time = current_milli_time()
@@ -48,9 +49,11 @@ def background_thread(sens_type, sens_time, sens_x, sens_y, sens_z):
                     namespace='/mynamespace',
                     broadcast=True)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
+
 
 @socketio.on('connect', namespace='/mynamespace')
 def connect():
@@ -60,14 +63,18 @@ def connect():
         'data': 'Connected'
     })
 
+
 @socketio.on('disconnect', namespace='/mynamespace')
 def disconnect():
     print "Disconnect"
 
+
 @socketio.on("request", namespace='/mynamespace')
 def request(sens_type, sens_time, sens_x, sens_y, sens_z):
     with thread_lock:
-        thread = socketio.start_background_task(background_thread, sens_type, sens_time, sens_x, sens_y, sens_z)
+        socketio.start_background_task(
+            background_thread, sens_type, sens_time, sens_x, sens_y, sens_z)
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
